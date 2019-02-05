5843521f4be33fba4dddecc8d89cf30d792e0256
olyuninv <olyuninv@tcd.ie>
2019-02-05 00:26:10 +0000

    chromatic

---
+++ Lab2/shaders/chromaticDispersion.fs
@@ -0,0 +1,53 @@
+#version 330 core
+#extension GL_NV_shadow_samplers_cube : enable
+
+in vec3 reflectedVector; 
+//in vec3 refractedVector; 
+in vec3 refractVecR;
+in vec3 refractVecG;
+in vec3 refractVecB;
+in vec3 Normal;  
+in vec3 FragPos;  
+in vec3 toCameraVector;
+
+uniform samplerCube skybox;
+uniform vec3 lightPos;
+uniform vec3 objectColor;
+
+out vec4 FragColor;
+
+void main()
+{    
+    // ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * vec3(1.0f, 1.0f, 1.0f);  // TODO: light color
+
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * vec3(1.0f, 1.0f, 1.0f); // TODO: light color
+    
+    vec3 result = (ambient + diffuse) * objectColor; 
+
+    FragColor = vec4(result, 1.0);   //NOT USED LATER
+
+    vec4 reflectedColour = texture(skybox, refractVecR);
+    //vec4 refractedColour = texture(skybox, refractedVector);  
+
+    vec4 color = vec4(0.0);
+    color.r = texture(skybox, refractVecR).r;  // ADDED
+    color.g = texture(skybox, refractVecG).g;  // ADDED
+    color.b = texture(skybox, refractVecB).b;  // ADDED
+    color.a = 1.0;
+
+
+    vec3 viewVector = normalize(toCameraVector);
+    float refractiveFactor = dot (viewVector, vec3 (0.0, 1.0, 0.0));
+    refractiveFactor = pow(refractiveFactor, 2.0);
+
+    vec4 environmentColour = mix(reflectedColour, color, refractiveFactor); //0.5f);  // ADDED
+
+    // FragColor = mix (environmentColour, vec4(0.0, 0.3, 0.5, 1.0), 0.2);
+    FragColor = mix(FragColor, environmentColour, 0.4f);  //refractedColour, 1.0); 
+}
---
+++ Lab2/shaders/chromaticDispersion.vs
@@ -0,0 +1,45 @@
+#version 330 core
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform vec3 viewPos;
+
+float iorRatioR = 1.5204;
+float iorRatioG = 1.5230;
+float iorRatioB = 1.5293;
+
+out vec4 clipSpace;
+out vec3 Normal;
+out vec3 FragPos;
+out vec3 toCameraVector;
+
+out vec3 reflectedVector; 
+//out vec3 refractedVector; 
+out vec3 refractVecR;
+out vec3 refractVecG;
+out vec3 refractVecB;
+
+void main()
+{
+    vec4 worldPosition = model * vec4(position, 1.0f);
+	clipSpace = projection * view *  worldPosition;	
+	gl_Position = clipSpace;
+	  
+	toCameraVector = viewPos -   worldPosition.xyz;
+
+	FragPos = vec3(model * vec4(position, 1.0f));
+    Normal = mat3(transpose(inverse(model))) * normal;
+    	
+	vec3 unitNormal = normalize (Normal);
+	
+    vec3 viewDirection = normalize(worldPosition.xyz - viewPos);
+            
+    reflectedVector = reflect (viewDirection, unitNormal);
+	//refractedVector = refract (viewDirection, unitNormal, 1.0/ 1.33);  // TODO: eta?
+	refractVecR = refract(viewDirection, unitNormal, iorRatioR);
+    refractVecG = refract(viewDirection, unitNormal, iorRatioG);
+	refractVecB = refract(viewDirection, unitNormal, iorRatioB);
+}
